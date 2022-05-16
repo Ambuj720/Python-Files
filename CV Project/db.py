e06fdd9d9cad168228d3478744051861f00d0e50
Ambuj720 <58222832+Ambuj720@users.noreply.github.com>
2022-05-16 13:07:24 +0530

    Create db.py

---
+++ CV Project/db.py
@@ -0,0 +1,25 @@
+from numpy import integer
+from sqlalchemy.engine import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float ,DateTime
+from datetime import date, datetime
+from sqlalchemy.orm import sessionmaker 
+
+Base = declarative_base()
+
+class ParkingSpace(Base):
+ __tablename__ = 'parking space'
+ id = Column(Integer,primary_key=True)
+ parking_area = Column(String,unique = True) 
+ free_space= Column(Integer , default = 0)
+ total_space = Column(Integer,default = 0) 
+ reserved_space=Column(Integer,default=0)
+ date= Column(DateTime,default = datetime.now)
+ time  = Column(DateTime,default = datetime.now)
+
+def __str__(self):
+        return self.title
+
+ 
+engine = create_engine("sqlite:///carpark.sqlite",echo = True)
+Base.metadata.create_all(engine)
